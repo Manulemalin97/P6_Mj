d669eac0f3dc866019336e6cfccd6e34b4a63747
unknown <m.jansen@outlook.fr>
2023-01-27 15:15:38 +0100

    mise en place de mes routes CRUD pour la création/suppression etc des sauces

---
+++ routes/sauce.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const multer = require ('../middleware/multer-config')
+//on importe notre middleware dans le router pour qu'il soit executé avant les gestionnaires de node
+const auth = require('../middleware/auth');
+
+const sauceCtrl = require("../controllers/sauce");
+
+//on mets auth avant les gestionnaires de route sinon ils seraient appellées en premier et sa n'aurait servi a rien
+router.get("/", auth, sauceCtrl.getAllSauce);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+
+
+
+
+//auth récupère d'abord les informations d'authentification, 
+//multer change le format de la requete,
+//
+
+
+module.exports = router;
